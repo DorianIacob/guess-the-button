299400dc5ede242313f2173e59393f28fd1093ff
DorianIacob <84712469+DorianIacob@users.noreply.github.com>
2021-06-21 09:32:47 +0300

    Generate random number before pressing a button
    Added a Start button that calls a function, that generate a random number (a winning button).
    Now the random number is generated before pressing one button.
    Also the random number, for 1'st play round is generated even before pressing the Start button. This allows the user to play the first round without boring him to press the Start button. But after the 1'st round he will have to hit the Start button to play another one.

--- guess-the-button/scripts/script-guess-the-button.js
+++ guess-the-button/scripts/script-guess-the-button.js
@@ -1,11 +1,19 @@
+// In case the user forget to press the Start button when playing the first round
+// he still will be able to play that round
+let winningButton = Math.floor(Math.random() * 3) + 1;
+
+// To play another (2'nd, 3'd and so on) round the user will have to press Start
+function generateRandomNumber() {
+  winningButton = Math.floor(Math.random() * 3) + 1;
+}
+
 function pressedButton(buttonId) {
-  let winningButton = Math.floor(Math.random() * 3) + 1;
   if (buttonId == winningButton) {
     document.getElementById("message1").textContent = ("You guessed! :)");
     document.getElementById("message1").style.color = "green";
     document.getElementById("message2").textContent = ("The winning button is " + winningButton);
   } else {
-    document.getElementById("message1").textContent = ("You didn't guessed! :)");
+    document.getElementById("message1").textContent = ("You didn't guessed! :(");
     document.getElementById("message1").style.color = "red";
     document.getElementById("message2").textContent = ("The winning button is " + winningButton);
   }
